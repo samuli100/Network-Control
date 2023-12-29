8b3e70258e9fc2e1b81d147412ab3c5af37bc709
samuli100 <78284472+samuli100@users.noreply.github.com>
2023-12-29 02:04:37 +0100

    Add files via upload

--- src/ncping.sh
+++ src/ncping.sh
@@ -29,25 +29,20 @@ pinging() {
         esac
     done < "$CONFIG_FILE"
 
-    if [ "$zeitintervall" -eq 0 ]; then
-        zeitintervall=1     # Should not be 0
-    fi
+    [[ $zeitintervall -eq 0 ]] && zeitintervall=1  # Should not be 0
 
     echo "---------- Start Pinging ----------"
 
     while IFS= read -r line || [ -n "$line" ]; do
-        if [ -n "$line" ]; then
-            echo "Ping will be executed for: $line"
+        [[ -n "$line" ]] || continue
 
-            ping -c "$anzahl_pakete" -w "$timeout" -i "$zeitintervall" "$line"
+        echo "Ping will be executed for: $line"
 
-            if [ $? -ne 0 ]; then
-                echo "$line not reachable!"
-                echo "$line" >> "$LOG_FILE"
-            fi
+        ping -c "$anzahl_pakete" -w "$timeout" -i "$zeitintervall" "$line"
 
-            echo "-------------------------"
-        fi
+        [[ $? -ne 0 ]] && { echo "$line not reachable!"; echo "$line" >> "$LOG_FILE"; }
+
+        echo "-------------------------"
     done < "$HOSTS_FILE"
 }
 
@@ -56,19 +51,15 @@ editConfig() {
 }
 
 addhost() {
-    if [ -n "$2" ]; then
-        echo "$2" >> "$HOSTS_FILE" && echo "$2 added successfully to default group!" || echo "Error adding host."
-    else
-        echo "No IP address provided."
-    fi
+    [[ -n "$2" ]] || { echo "No IP address provided."; return 1; }
+
+    echo "$2" >> "$HOSTS_FILE" && echo "$2 added successfully to default group!" || echo "Error adding host."
 }
 
 delhost(){
-    if [ -n "$2" ]; then
-        sed -i "/$2/d" "$HOSTS_FILE" && echo "$2 deleted successfully!" || echo "Error deleting host."
-    else
-        echo "No IP address provided."
-    fi
+    [[ -n "$2" ]] || { echo "No IP address provided."; return 1; }
+
+    sed -i "/$2/d" "$HOSTS_FILE" && echo "$2 deleted successfully!" || echo "Error deleting host."
 }
 
 printhosts(){
@@ -77,116 +68,71 @@ printhosts(){
 }
 
 creategroup() {
-    if [ -n "$2" ]; then
-        echo "$2" >> "$GROUPS_FILE" && echo "Group $2 created successfully!" || echo "Error creating group."
-    else
-        echo "No group name provided."
-    fi
+    [[ -n "$2" ]] || { echo "No group name provided."; return 1; }
+
+    echo "$2" >> "$GROUPS_FILE" && echo "Group $2 created successfully!" || echo "Error creating group."
 }
 
 delgroup() {
-    if [ -n "$2" ]; then
-        sed -i "/$2/d" "$GROUPS_FILE" && echo "Group $2 deleted successfully!" || echo "Error deleting group."
-    else
-        echo "No group name provided."
-    fi
+    [[ -n "$2" ]] || { echo "No group name provided."; return 1; }
+
+    sed -i "/$2/d" "$GROUPS_FILE" && echo "Group $2 deleted successfully!" || echo "Error deleting group."
 }
 
 addgroup() {
-    if [ -n "$2" ] && [ -n "$3" ]; then
-        echo "$2 $3" >> "$GROUPS_FILE" && echo "$3 added to group $2 successfully!" || echo "Error adding host to group."
-    else
-        echo "Usage: ncping addgroup GROUP_NAME IP_ADDRESS"
-    fi
+    [[ -n "$2" && -n "$3" ]] || { echo "Usage: ncping addgroup GROUP_NAME IP_ADDRESS"; return 1; }
+
+    echo "$2 $3" >> "$GROUPS_FILE" && echo "$3 added to group $2 successfully!" || echo "Error adding host to group."
 }
 
 rmgroup() {
-    if [ -n "$2" ] && [ -n "$3" ]; then
-        sed -i "/$2 $3/d" "$GROUPS_FILE" && echo "$3 removed from group $2 successfully!" || echo "Error removing host from group."
-    else
-        echo "Usage: ncping rmgroup GROUP_NAME IP_ADDRESS"
-    fi
+    [[ -n "$2" && -n "$3" ]] || { echo "Usage: ncping rmgroup GROUP_NAME IP_ADDRESS"; return 1; }
+
+    sed -i "/$2 $3/d" "$GROUPS_FILE" && echo "$3 removed from group $2 successfully!" || echo "Error removing host from group."
 }
 
 ccron() {
-    if [ -n "$2" ]; then
-        local group_name="$2"
-        local cron_schedule="$3"
+    [[ -n "$2" ]] || { echo "Usage: ncping ccron GROUP_NAME CRON_SCHEDULE"; return 1; }
 
-        if grep -qE "^$group_name " "$GROUPS_FILE"; then
-            local cron_job="*/$cron_schedule * * * * /usr/share/ncping/ncping.sh $group_name"
+    local group_name="$2"
+    local cron_schedule="$3"
 
-            (crontab -l ; echo "$cron_job") | crontab - && echo "Cron job added successfully for group $group_name."
-            echo "$cron_job" >> "$CRON_JOBS_FILE"
-        else
-            echo "Group $group_name does not exist."
-        fi
+    if grep -qE "^$group_name " "$GROUPS_FILE"; then
+        local cron_job="*/$cron_schedule * * * * /bin/bash -c '/usr/share/ncping/ncping.sh $group_name'"
+
+        (crontab -l ; echo "$cron_job") | crontab - && echo "Cron job added successfully for group $group_name."
+        echo "$cron_job" >> "$CRON_JOBS_FILE"
     else
-        echo "Usage: ncping ccron GROUP_NAME CRON_SCHEDULE"
+        echo "Group $group_name does not exist."
     fi
 }
 
 delcron() {
-    if [ -n "$2" ]; then
-        local group_name="$2"
+    [[ -n "$2" ]] || { echo "Usage: ncping delcron GROUP_NAME"; return 1; }
 
-        if grep -qE "^$group_name " "$GROUPS_FILE"; then
-            local cron_job_pattern=".*$CRON_JOB_NAME.sh $group_name"
-            local crontab_content=$(crontab -l 2>/dev/null || echo "")
+    local group_name="$2"
 
-            if echo "$crontab_content" | grep -qE "$cron_job_pattern"; then
-                echo "$crontab_content" | sed -E "/$cron_job_pattern/d" | crontab - && echo "Cron job removed successfully for group $group_name."
-                sed -i "/$group_name/d" "$CRON_JOBS_FILE"
-            else
-                echo "No corresponding Cron job found for group $group_name."
-            fi
+    if grep -qE "^$group_name " "$GROUPS_FILE"; then
+        local cron_job_pattern=".*$CRON_JOB_NAME.sh $group_name"
+        local crontab_content=$(crontab -l 2>/dev/null || echo "")
+
+        if echo "$crontab_content" | grep -qE "$cron_job_pattern"; then
+            echo "$crontab_content" | sed -E "/$cron_job_pattern/d" | crontab - && echo "Cron job removed successfully for group $group_name."
+            sed -i "/$group_name/d" "$CRON_JOBS_FILE"
         else
-            echo "Group $group_name does not exist."
+            echo "No corresponding Cron job found for group $group_name."
         fi
     else
-        echo "Usage: ncping delcron GROUP_NAME"
+        echo "Group $group_name does not exist."
     fi
 }
 
 case "$1" in
-    "config")
-        editConfig
-        ;;
-    "addhost")
-        addhost "$@"
-        ;;
-    "delhost")
-        delhost "$@"
-        ;;
-    "lshosts")
-        printhosts
-        ;;
-    "creategroup")
-        creategroup "$@"
-        ;;
-    "delgroup")
-        delgroup "$@"
-        ;;
-    "addgroup")
-        addgroup "$@"
-        ;;
-    "rmgroup")
-        rmgroup "$@"
-        ;;
-    "ccron")
-        ccron "$@"
-        ;;
-    "delcron")
-        delcron "$@"
+    "config" | "addhost" | "delhost" | "lshosts" | "creategroup" | "delgroup" | "addgroup" | "rmgroup" | "ccron" | "delcron")
+        "$1" "$@"
         ;;
     "-h")
         man ncping
-        ;;
-    "install")
-        install "$@"
-        ;;
-    "uninstall")
-        uninstall
         ;;
     *)
         pinging
--- uninstall.sh
+++ uninstall.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
-SCRIPT_NAME="ncping"
-INSTALL_DIR="/usr/share/$SCRIPT_NAME"
+INSTALL_DIR="/usr/share/ncping"
 BIN_DIR="/usr/bin"
 MAN_DIR="/usr/local/share/man/man1"
+CRON_JOBS_FILE="/usr/share/ncping/cronjobs"
 
 # Remove script directory + contents
 rm -rf "$INSTALL_DIR" || { echo "Error removing script directory."; exit 1; }
 
 # Remove script link
-rm "$BIN_DIR/$SCRIPT_NAME" || { echo "Error removing script link."; exit 1; }
+rm "$BIN_DIR/ncping" || { echo "Error removing script link."; exit 1; }
 
 # Remove man page
-rm "$MAN_DIR/$SCRIPT_NAME.1.gz" || { echo "Error removing man page."; exit 1; }
+rm "$MAN_DIR/ncping.1.gz" || { echo "Error removing man page."; exit 1; }
 
 # Verify removal of script directory
 if [ ! -d "$INSTALL_DIR" ]; then
@@ -22,14 +22,14 @@ else
 fi
 
 # Verify removal script link
-if [ ! -f "$BIN_DIR/$SCRIPT_NAME" ]; then
+if [ ! -f "$BIN_DIR/ncping" ]; then
     echo "Script link removed."
 else 
-    echo "Error removing script  link."
+    echo "Error removing script link."
 fi
 
 # Verify removal of man page
-if [ ! -f "$MAN_DIR/$SCRIPT_NAME.1.gz" ]; then
+if [ ! -f "$MAN_DIR/ncping.1.gz" ]; then
     echo "Man page removed."
 else
     echo "Error removing man page."
@@ -37,11 +37,21 @@ fi
 
 # Remove cron job
 crontab_content=$(crontab -l 2>/dev/null || echo "")
-job_pattern=".*$SCRIPT_NAME.sh"
+job_pattern=".*ncping.sh"
 
 if echo "$crontab_content" | grep -qE "$job_pattern"; then
     echo "$crontab_content" | sed -E "/$job_pattern/d" | crontab -
-    echo "Cron job removed."
+    echo "Cron job removed"
 else
-    echo "No cron job found for $SCRIPT_NAME."
+    echo "No corresponding Cron job found for ncping"
+fi
+
+# Remove cron jobs file
+rm -f "$CRON_JOBS_FILE" || { echo "Error removing cron jobs file."; exit 1; }
+
+# Verify removal of cron jobs file
+if [ ! -f "$CRON_JOBS_FILE" ]; then
+    echo "Cron jobs file removed."
+else
+    echo "Error removing cron jobs file."
 fi
