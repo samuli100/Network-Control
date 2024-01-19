1be3708b35bb51967b1a7ff4a870ef059aa2fb0d
samuli100 <78284472+samuli100@users.noreply.github.com>
2024-01-19 17:49:13 +0100

    Add files via upload

--- uninstall.sh
+++ uninstall.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
-CRON_JOB_NAME="ncping"
-SCRIPT_DIR="/usr/share/ncping"
+SCRIPT_NAME="ncping"
+SCRIPT_NAME_ger="ncping_ger"
+SCRIPT_DIR="/usr/share/$SCRIPT_NAME"
 BIN_DIR="/usr/bin"
 MAN_DIR="/usr/local/share/man/man1"
 
 # Remove cron job
-cron_job_pattern=".*$CRON_JOB_NAME"
+cron_job_pattern=".*$SCRIPT_NAME"
 crontab_content=$(crontab -l 2>/dev/null || echo "")
 
 if echo "$crontab_content" | grep -qE "$cron_job_pattern"; then
-    echo "$crontab_content" | sed -E "/$cron_job_pattern/d" | crontab - && echo "Cron job removed successfully: $CRON_JOB_NAME."
+    echo "$crontab_content" | sed -E "/$cron_job_pattern/d" | crontab - && echo "Cron job removed successfully: $SCRIPT_NAME."
 else
-    echo "No corresponding Cron job found: $CRON_JOB_NAME."
+    echo "No corresponding Cron job found: $SCRIPT_NAME."
 fi
 
 # Add debug information
@@ -22,23 +23,16 @@ echo "Removing script directory: $SCRIPT_DIR"
 rm -rf "$SCRIPT_DIR" || { echo "Error removing script directory."; exit 1; }
 
 # Remove script link
-rm "$BIN_DIR/$CRON_JOB_NAME" || { echo "Error removing script link."; exit 1; }
+rm "$BIN_DIR/$SCRIPT_NAME" || { echo "Error removing script link."; exit 1; }
 
 # Remove man page
-rm "$MAN_DIR/$CRON_JOB_NAME.1.gz" || { echo "Error removing man page."; exit 1; }
-
-# Remove the Network-Control folder
-NETWORK_CONTROL_DIR=$(readlink -f "$0")
-rm -rf "$NETWORK_CONTROL_DIR" || { echo "Error removing Network-Control folder."; exit 1; }
+rm "$MAN_DIR/$SCRIPT_NAME.1.gz" "$MAN_DIR/$SCRIPT_NAME_ger.1.gz" || { echo "Error removing man pages."; exit 1; }
 
 # Verify removal of script directory
 [ ! -d "$SCRIPT_DIR" ] && echo "Program directory removed." || echo "Error removing program directory."
 
 # Verify removal script link
-[ ! -f "$BIN_DIR/$CRON_JOB_NAME" ] && echo "Script link removed." || echo "Error removing script link."
+[ ! -f "$BIN_DIR/$SCRIPT_NAME" ] && echo "Script link removed." || echo "Error removing script link."
 
-# Verify removal of man page
-[ ! -f "$MAN_DIR/$CRON_JOB_NAME.1.gz" ] && echo "Man page removed." || echo "Error removing man page."
-
-# Verify removal of Network-Control folder
-[ ! -d "$NETWORK_CONTROL_DIR" ] && echo "Network-Control folder removed." || echo "Error removing Network-Control folder."
+# Verify removal of man pages
+[ ! -f "$MAN_DIR/$SCRIPT_NAME.1.gz" ] && [ ! -f "$MAN_DIR/$SCRIPT_NAME_ger.1.gz" ] && echo "Man pages removed." || echo "Error removing man pages."
